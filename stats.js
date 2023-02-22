a6689e2b1b187e9224135ef8d14c5dac699d67b6
Evandro Florscuk <evandro_ff@me.com>
2023-02-22 00:56:39 -0300

    end stats

---
+++ stats.js
@@ -0,0 +1,27 @@
+const os = require('os');
+
+// aplicação para mostrar memória livre, memória total e porcentagem de uso das memórias
+
+setInterval(() => {
+    const { freemem, totalmem } = os;
+    
+    //console.log(`${parseInt(freemem() / 1024 / 1024)} MB`, totalmem() / 1024 / 1024);
+    
+    const total = parseInt(totalmem() / 1024 / 1024);
+    const mem = parseInt(freemem() / 1024 / 1024);
+    const percents = parseInt((mem / total) * 100);
+    
+    //console.log(`=> ${mem}, => ${total}, => ${percents}`);
+    
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`,
+    }
+    
+    console.clear();
+    console.log("=== PC STATS ===");
+    console.table(stats);
+}, 1000);
+
+
